6050c757da51ab0e589499b0010a7abcf1877f8c
Bharath200415 <bharath200415@gmail.com>
2026-04-06 01:33:54 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
